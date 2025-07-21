ee85dcdaefbf7a122e5c84943af4aa91343de8a0
jesselyn <jesselynva@gmail.com>
2025-07-21 12:35:15 +0700

    envelope animation

--- app/Components/Envelope.js
+++ app/Components/Envelope.js
@@ -1,9 +1,26 @@
 'use-client';
-import React from "react";
+import React, {useState} from "react";
 import Spline from "@splinetool/react-spline";
 
 export default function Envelope() {
-  return (
-    <Spline scene="https://prod.spline.design/QwCKxSuek9YolkQb/scene.splinecode" />
-  );
+    const [sceneUrl, setSceneUrl] = useState("https://prod.spline.design/QwCKxSuek9YolkQb/scene.splinecode");
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleClick = () => {
+        if(!isFlipped) {
+            setIsFlipped(true);
+        } else {
+            setSceneUrl("https://prod.spline.design/RRF-H-e5ZfciGKxs/scene.splinecode");
+        }
+    }
+    return (
+        <div 
+            onClick={handleClick} 
+            style={{cursor: "pointer"}}
+        >
+            <Spline
+                scene = {sceneUrl}
+            /> 
+        </div>
+    );
 }
