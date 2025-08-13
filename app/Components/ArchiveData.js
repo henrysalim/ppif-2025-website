aa56319f24b9006457974beb72f6b1bbf54d63e0
Felicia Sabatini <feliciasabatinigunawan2005@gmail.com>
2025-08-13 12:43:56 +0700

    add: coming soon remove: placeholder archivedata

--- app/Components/ArchiveData.js
+++ app/Components/ArchiveData.js
@@ -1,84 +1,11 @@
 export const BriefingDay = [
-    {
-        title: "Astronaut",
-        src: "/Images/tes/astr.jpg"
-    },
-    {
-        title: "Space Man",
-        src: "/Images/tes/anotha space man.jpg"
-    },
-    {
-        title: "Earth",
-        src: "/Images/tes/earth.jpg"
-    },
-    {
-        title: "Mansion",
-        src: "/Images/tes/Mansion.png"
-    },
-    {
-        title: "Arena",
-        src: "/Images/tes/Arena.png"
-    },
-    {
-        title: "Corrupted Image",
-        src: "/Images/AssetArchives/Group33.png"
-    },
-    {
-        title: "Corrupted Image",
-        src: "/Images/AssetArchives/Group33.png"
-    },
-    {
-        title: "Corrupted Image",
-        src: "/Images/AssetArchives/Group33.png"
-    },
-    {
-        title: "Corrupted Image",
-        src: "/Images/AssetArchives/Group33.png"
-    },
-    {
-        title: "Corrupted Image",
-        src: "/Images/AssetArchives/Group33.png"
-    },
-    {
-        title: "Corrupted Image",
-        src: "/Images/AssetArchives/Group33.png"
-    },
-    {
-        title: "Corrupted Image",
-        src: "/Images/AssetArchives/Group33.png"
-    },
-    {
-        title: "Corrupted Image",
-        src: "/Images/AssetArchives/Group33.png"
-    }
+    
 ];
 
 export const DDay = [
-    {
-        title: "Astronaut",
-        src: "/Images/tes/astr.jpg"
-    },
-    {
-        title: "Earth",
-        src: "/Images/tes/earth.jpg"
-    },
-    {
-        title: "Space Man",
-        src: "/Images/tes/anotha space man.jpg"
-    }
+   
 ];
 
 export const DominationDay = [
-    {
-        title: "Earth",
-        src: "/Images/tes/earth.jpg"
-    },
-    {
-        title: "Astronaut",
-        src: "/Images/tes/astr.jpg"
-    },
-    {
-        title: "Space Man",
-        src: "/Images/tes/anotha space man.jpg"
-    }
+    
 ];
