cb5c9c715e9c6d58e8333bfd0bd10206d39b0fcf
Henry Salim <henrysalim@Henrys-MacBook-Air.local>
2025-08-13 18:11:41 +0700

    feat: added divisions photo, title, and skills

--- app/Components/swiperData.js
+++ app/Components/swiperData.js
@@ -3,29 +3,21 @@ const slides = [
         {
             id: 1,
             divisionimg: '/Images/Div-Logo/Administrator.webp',
-            img: '/Images/dummy_test/img_01.webp',
-            title: 'Zenless Combat',
+            img: '/Images/Divisions/Admin.webp',
+            title: 'ADMINISTRATOR',
             description: 'Dynamic in-game combat showcase from Zenless Zone Zero.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
-                { name: 'Leadership', value: 90 },
+                { name: 'Leadership', value: 85 },
                 { name: 'Documentation', value: 80 },
-                { name: 'Organization', value: 85 }
+                { name: 'Organization', value: 95 }
             ]
         },
         {
             id: 2,
             divisionimg: '/Images/Div-Logo/Bitmap.webp',
-            img: '/Images/dummy_test/img_02.webp',
-            title: 'Underground Cityscape',
+            img: '/Images/Divisions/Bitmap.webp',
+            title: 'BITMAP',
             description: 'A gritty alley view from the underworld city.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
                 { name: 'Pixel Art', value: 95 },
                 { name: 'Animation', value: 80 },
@@ -35,13 +27,9 @@ const slides = [
         {
             id: 3,
             divisionimg: '/Images/Div-Logo/Console.webp',
-            img: '/Images/dummy_test/img_03.webp',
-            title: 'Main Protagonist',
+            img: '/Images/Divisions/Console.webp',
+            title: 'CONSOLE',
             description: 'Character portrait from the main storyline.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
                 { name: 'Game Logic', value: 88 },
                 { name: 'Debugging', value: 92 },
@@ -53,45 +41,33 @@ const slides = [
         {
             id: 4,
             divisionimg: '/Images/Div-Logo/Developer.webp',
-            img: '/Images/dummy_test/img_04.webp',
-            title: 'Urban Combat Zone',
+            img: '/Images/Divisions/Developer.webp',
+            title: 'DEVELOPER',
             description: 'A character stands amidst chaos, ready for battle in the urban ruins.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
-                { name: 'Leadership', value: 90 },
+                { name: 'Leadership', value: 95 },
                 { name: 'Documentation', value: 80 },
-                { name: 'Organization', value: 85 }
+                { name: 'Organization', value: 90 }
             ]
         },
         {
             id: 5,
             divisionimg: '/Images/Div-Logo/Encryptor.webp',
-            img: '/Images/dummy_test/img_05.webp',
-            title: 'Street Market Scene',
+            img: '/Images/Divisions/Encryptor.webp',
+            title: 'ENCRYPTOR',
             description: 'Bright lights and vibrant colors dominate this busy market street.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
                 { name: 'Leadership', value: 90 },
                 { name: 'Documentation', value: 80 },
-                { name: 'Organization', value: 85 }
+                { name: 'Organization', value: 90 }
             ]
         },
         {
             id: 6,
             divisionimg: '/Images/Div-Logo/Extension.webp',
-            img: '/Images/dummy_test/img_06.webp',
-            title: 'Cyberpunk Alley',
+            img: '/Images/Divisions/Extension.webp',
+            title: 'EXTENSION',
             description: 'A neon-lit alleyway straight out of a futuristic cityscape.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
                 { name: 'Leadership', value: 90 },
                 { name: 'Documentation', value: 80 },
@@ -103,48 +79,36 @@ const slides = [
         {
             id: 7,
             divisionimg: '/Images/Div-Logo/Modul.webp',
-            img: '/Images/dummy_test/img_07.webp',
-            title: 'Team Pose',
+            img: '/Images/Divisions/Module.webp',
+            title: 'MODULE',
             description: 'The squad assembles for a group shot before diving into action.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
-                { name: 'Leadership', value: 90 },
+                { name: 'Leadership', value: 92 },
                 { name: 'Documentation', value: 80 },
-                { name: 'Organization', value: 85 }
+                { name: 'Organization', value: 90 }
             ]
         },
         {
             id: 8,
             divisionimg: '/Images/Div-Logo/ProjectManager.webp',
-            img: '/Images/dummy_test/img_08.webp',
-            title: 'Dynamic Duo',
+            img: '/Images/Divisions/ProjectManager.webp',
+            title: 'PROJECT MANAGER',
             description: 'Two central characters showcase power and chemistry in this scene.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
-                { name: 'Leadership', value: 90 },
+                { name: 'Leadership', value: 95 },
                 { name: 'Documentation', value: 80 },
-                { name: 'Organization', value: 85 }
+                { name: 'Organization', value: 95 }
             ]
         },
         {
             id: 9,
             divisionimg: '/Images/Div-Logo/Snapshot.webp',
-            img: '/Images/dummy_test/img_09.webp',
-            title: 'City Overlook',
+            img: '/Images/Divisions/Snapshot.webp',
+            title: 'SNAPSHOT',
             description: 'A sweeping view of the city at dusk with cyberpunk aesthetics.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
-                { name: 'Leadership', value: 90 },
-                { name: 'Documentation', value: 80 },
+                { name: 'Leadership', value: 85 },
+                { name: 'Documentation', value: 90 },
                 { name: 'Organization', value: 85 }
             ]
         }
@@ -153,14 +117,10 @@ const slides = [
         {
             id: 10,
             divisionimg: '/Images/Div-Logo/Website.webp',
-            img: '/Images/dummy_test/img_01.webp',
-            title: 'Zenless Combat',
+            img: '/Images/Divisions/Website.webp',
+            title: 'WEBSITE',
 
             description: 'Dynamic in-game combat showcase from Zenless Zone Zero.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
                 { name: 'Programming', value: 93 },
                 { name: 'Problem Solving', value: 87 },
@@ -170,13 +130,9 @@ const slides = [
         {
             id: 11,
             divisionimg: '/Images/Div-Logo/Null.png',
-            img: '/Images/dummy_test/img_02.webp',
+            img: '/Images/Divisions/img_02.webp',
             title: 'Underground Cityscape',
             description: 'A gritty alley view from the underworld city.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
                 { name: 'Leadership', value: 90 },
                 { name: 'Documentation', value: 80 },
@@ -186,13 +142,9 @@ const slides = [
         {
             id: 12,
             divisionimg: '/Images/Div-Logo/Null.png',
-            img: '/Images/dummy_test/img_03.webp',
+            img: '/Images/Divisions/img_03.webp',
             title: 'Main Protagonist',
             description: 'Character portrait from the main storyline.',
-            teamMembers: [
-                { name: 'Yuzuha', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/6/6d/Agent_Ukinami_Yuzuha_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250716024914' },
-                { name: 'Vivian', photo: 'https://static.wikia.nocookie.net/zenless-zone-zero/images/f/f5/Agent_Vivian_Banshee_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20250423003356' }
-            ],
             skills: [
                 { name: 'Leadership', value: 90 },
                 { name: 'Documentation', value: 80 },
@@ -204,7 +156,7 @@ const slides = [
     //     {
     //         id: 13,
     //         divisionimg: '/Images/Div-Logo/Null.png',
-    //         img: '/Images/dummy_test/img_07.webp',
+    //         img: '/Images/Divisions/img_07.webp',
     //         title: 'Team Pose',
     //         description: 'The squad assembles for a group shot before diving into action.',
     //         teamMembers: [
@@ -220,7 +172,7 @@ const slides = [
     //     {
     //         id: 14,
     //         divisionimg: '/Images/Div-Logo/Null.png',
-    //         img: '/Images/dummy_test/img_08.webp',
+    //         img: '/Images/Divisions/img_08.webp',
     //         title: 'Dynamic Duo',
     //         description: 'Two central characters showcase power and chemistry in this scene.',
     //         teamMembers: [
@@ -236,7 +188,7 @@ const slides = [
     //     {
     //         id: 15,
     //         divisionimg: '/Images/Div-Logo/Null.png',
-    //         img: '/Images/dummy_test/img_09.webp',
+    //         img: '/Images/Divisions/img_09.webp',
     //         title: 'City Overlook',
     //         description: 'A sweeping view of the city at dusk with cyberpunk aesthetics.',
     //         teamMembers: [
